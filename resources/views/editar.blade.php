4db102f1a7386174fa42f59b63dcb25f799394b1
ThiagoMartins11 <thiago.marques29@etec.sp.gov.br>
2023-05-12 15:34:12 -0300

    CRUD

---
+++ resources/views/editar.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+@vite([
+            'resources/css/app.css', 
+                'resources/js/app.js',
+                'node_modules/bootstrap/dist/css/bootstrap.min.css',
+                'node_modules/bootstrap/dist/js/bootstrap.bundle.js'
+            ])
+
+<style>
+body{
+    background-color: #d3d3d3;
+}
+	
+
+
+
+</style>
+
+	<title>SISTEMA DE AGENDAMENTO - CLIENTES</title>
+</head> 
+
+<body> 
+	<div class="container">
+		<div class="row">
+			<nav class="navbar navbar-expand-lg navbar-dark bg-primary col-12">
+				<a class="navbar-brand" href="#">&nbsp;&nbsp;&nbsp;SISTEMA WEB</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNavDropdown">
+					<ul class="navbar-nav">
+						<li class="nav-item active">
+							<a class="nav-link" href="/">Cadastrar</a>
+						</li>
+                        <br>
+						<li class="nav-item">
+							<a class="nav-link" href="consulta ">Consultar</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+		<div class="row">
+			<div class="card mb-3 col-12">
+				<div class="card-body">
+					<h5 class="card-title">Cadastrar - Agendamento de Potenciais Clientes</h5>
+					<p class="card-text">Sistema utilizado para agendamento de serviços.</p>
+					<p>
+						<form method="post" action="/atualizar?agendamento={{$agendamento->id}}" id="form" name="form">
+							@csrf
+							<div class="form-group">
+								<label for="exampleFormControlInput1">Nome:</label>
+								<input value="{{$agendamento->nome}}" type="text" class="form-control" name="txtNome" required id="txtNome">
+                                <br>
+							</div>
+							<div class="form-group">
+								<label for="exampleFormControlInput1">Telefone:</label>
+								<input value="{{$agendamento->telefone}}" type="tel" class="form-control" required name="txtTelefone" id="txtTelefone" placeholder="(xx)xxxxx-xxxx">
+                                <br>
+							</div>
+							<div class="form-group">
+								<label for="exampleFormControlSelect1">Origem:</label>
+                                <br>
+								<select class="form-control" required name="txtOrigem" id="txtOrigem">
+									<option>Celular</option>
+									<option>Fixo</option>
+									<option>Whatsapp</option>
+									<option>Facebook</option>
+									<option>Instagram</option>
+									<option>Google Meu Negocio</option>
+								</select>
+							</div>
+                            <br>
+							<div class="form-group">
+								<label for="exampleFormControlInput1">Data do Contato:</label>
+								<input value="{{$agendamento->data_contato}}" type="date" class="form-control" required name="txtDataContato" id="txtDataContato">
+                                <br>
+							</div>
+							<div class="form-group">
+								<label for="exampleFormControlTextarea1">Observação</label>
+								<textarea class="form-control" name="txtObservacao" id="txtObservacao" rows="3">{{$agendamento->observacao}}</textarea>
+							</div>
+                            <br>
+							<button type="submit" id="btnInserir" class="btn btn-primary">Cadastrar</button>
+						</form>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
